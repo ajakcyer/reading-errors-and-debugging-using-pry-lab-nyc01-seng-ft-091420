ed5d4b201c4212e08900df307b623ccc128c093a
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-02 18:56:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -11,7 +11,8 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engagebinding.pry
+def engage
+  binding.pry
   puts state_log(star_date)
   
   date = generate_star_date
